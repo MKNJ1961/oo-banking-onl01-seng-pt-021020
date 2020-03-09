ce33df152d57d3d5e5909544bad850f082ef9144
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-09 23:57:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,6 @@
+require'pry'
+
+
 class Transfer
 
     attr_accessor :sender, :receiver, :amount, :status
@@ -21,6 +24,7 @@ class Transfer
       @sender.balance -= @amount
       @receiver.balance += @amount
       @status = "complete"
+      binding.pry
     else
       @status = "rejected"
       return "Transaction rejected. Please check your account balance"
