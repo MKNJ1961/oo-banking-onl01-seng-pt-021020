3f207e685c5834fab5e9518e6b5e02c5cadf9d6f
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-09 23:50:54 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -18,7 +18,7 @@ class Transfer
   def execute_transaction
     if
     @sender.balance > @amount && @status == "pending"
-    @sender.balance += & @amount
+    @sender.balance += @amount
     @receiver.balance += @amount
     @status = "complete"
   else
