9a62ac5494a4d0dfb5fed909374d5ff69e1e5795
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-09 23:50:32 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,8 +16,14 @@ class Transfer
   end
 
   def execute_transaction
+    if
     @sender.balance > @amount && @status == "pending"
     @sender.balance += & @amount
-    @receiver.balance += @amount@status = "complete"
+    @receiver.balance += @amount
+    @status = "complete"
+  else
+    @status = "rejected"
+    return "Transaction rejected. Please check your account balance"
+  end
   end
 end
