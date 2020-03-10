b70f7ed6d39f1a0292d47ca6880633e179260ec1
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-10 00:03:42 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -28,7 +28,7 @@ class Transfer
 
   def reverse_transfer
     if
-       @status == "complete"
+      @status == "complete"
       @sender.balance += @amount
       @receiver.balance -= @amount
       @status = "reversed"
