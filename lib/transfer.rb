e819d18972d8149630686777cbacd0329584e083
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-10 00:11:21 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,16 +15,27 @@ class Transfer
   end
 
   def execute_transaction
-    if
-      @sender.balance > @amount && @status == "pending"
-      @sender.balance -= @amount
-      @receiver.balance += @amount
-      @status = "complete"
-    else
-      @status = "rejected"
-      return "Transaction rejected. Please check your account balance"
-    end
-  end
+   if(self.status != "pending")
+   elsif !self.valid?
+     @status = "rejected"
+     "Transaction rejected. Please check your account balance."
+   else
+     receiver.deposit(amount)
+     sender.deposit(-amount)
+     @status = "complete"
+   end
+ end
+  # def execute_transaction
+  #   if
+  #     @sender.balance > @amount && @status == "pending"
+  #     @sender.balance -= @amount
+  #     @receiver.balance += @amount
+  #     @status = "complete"
+  #   else
+  #     @status = "rejected"
+  #     return "Transaction rejected. Please check your account balance"
+  #   end
+  # end
 
   def reverse_transfer
     if
